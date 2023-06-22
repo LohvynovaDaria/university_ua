5eaeafdf1813de3cdd8291cdb5201f27b08a40ee
LohvynovaDaria <136909967+LohvynovaDaria@users.noreply.github.com>
2023-06-22 15:25:54 +0200

    Create task_10.js

---
+++ javascript/task_10.js
@@ -0,0 +1,9 @@
+(function() {
+    let parent = document.getElementById('test');
+    let child1 = document.createElement('li');
+    let child2 = document.createElement('li');
+    let child3 = document.createElement('li');
+    parent.appendChild(child1);
+    parent.appendChild(child2);
+    parent.appendChild(child3);
+})()
